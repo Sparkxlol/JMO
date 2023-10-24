c33a9aeb945fa35572b871856f91c856da8fa702
Sparkxlol <sparkxmkw@gmail.com>
2023-10-23 21:51:46 -0500

    testing

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -8,6 +8,7 @@ public class Motion : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     [SerializeField] private float speed;
+    [SerializeField] private GameObject sword;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +23,27 @@ public class Motion : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical"));
 
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GameObject createdSword = Instantiate(sword, new Vector3(transform.position.x + 5, transform.position.y, 0), transform.rotation);
+            Destroy(createdSword, 1f);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Hi");
-            rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
+            StartCoroutine(Jump());
         }
     }
 
     private void FixedUpdate()
     {
         
+    }
+
+    IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(1f);
+        rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
     }
 }
